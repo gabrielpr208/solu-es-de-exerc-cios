dc95e99d8919aa2ab65650bcc8494589046061f7
gabrielpr208 <gabriel.rodrigues.gpr@gmail.com>
2024-10-24 16:47:27 -0300

    Add files via upload

---
+++ beecrowd 1013.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ int a, b, c, x, m;
+		
+	scanf("%d %d %d", &a, &b, &c);
+	
+	x=(a+b+abs(a-b))/2;
+	m=(x+c+abs(x-c))/2;
+	
+	printf("%d eh o maior\n", m);
+	
+    return 0;
+}
